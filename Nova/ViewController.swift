debf19b1f6ff692e29580bb885b63fdb4fb236ef
Andrej Jurkin <andrej.jurkin@gmail.com>
2017-10-02 23:28:48 +0200

    Init project

---
+++ Nova/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nova
+//
+//  Created by Andrej Jurkin on 10/2/17.
+//  Copyright © 2017 Andrej Jurkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
